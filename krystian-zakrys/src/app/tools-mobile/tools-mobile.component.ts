202e79850f521a050ba74f9edef467d8b6b4159e
KrystianZakrys <krystian.zakrys@gmail.com>
2025-01-20 16:52:49 +0100

    Theme and social media

--- krystian-zakrys/src/app/tools-mobile/tools-mobile.component.ts
+++ krystian-zakrys/src/app/tools-mobile/tools-mobile.component.ts
@@ -9,34 +9,28 @@ export interface Tool {
   styleUrls: ['./tools-mobile.component.scss']
 })
 export class ToolsMobileComponent implements OnInit {
- tools: Tool[] = [
-    { imageUrl: '../../assets/tools/figma.svg', name: 'Figma' },
-    { imageUrl: '../../assets/tools/visual-studio.svg', name: 'Visual Studio' },
-    { imageUrl: '../../assets/tools/visual-studio-code.svg', name: 'Visual Studio Code' },
-    { imageUrl: '../../assets/tools/git.svg', name: 'Git' },
-    { imageUrl: '../../assets/tools/github.svg', name: 'GitHub' },
-    { imageUrl: '../../assets/tools/bitbucket.png', name: 'BitBucket' },
-    { imageUrl: '../../assets/tools/jira.svg', name: 'Jira' },
-    { imageUrl: '../../assets/tools/confluence.svg', name: 'Confluence' },
-    { imageUrl: '../../assets/tools/azure-data-studio.svg', name: 'Azure Data Studio' },
-    { imageUrl: '../../assets/tools/vmware-horizon.png', name: 'VMware Horizon' },
-    { imageUrl: '../../assets/tools/azure-devops.png', name: 'Azure DevOps' },
-    { imageUrl: '../../assets/tools/azure.svg', name: 'Azure' },
-    { imageUrl: '../../assets/tools/notion.svg', name: 'Notion' },
-    { imageUrl: '../../assets/tools/figma.svg', name: 'Figma' },
-    { imageUrl: '../../assets/tools/mirro.svg', name: 'Mirro' },
-    { imageUrl: '../../assets/tools/iis.png', name: 'IIS' },
-    { imageUrl: '../../assets/tools/teams.svg', name: 'Teams' },
-    { imageUrl: '../../assets/tools/npm.svg', name: 'npm' },
-    { imageUrl: '../../assets/tools/jenkins.svg', name: 'Jenkins' },
-    { imageUrl: '../../assets/tools/docker.svg', name: 'Docker' },
-    { imageUrl: '../../assets/tools/figma.svg', name: 'Figma' },
-    { imageUrl: '../../assets/tools/visual-studio.svg', name: 'Visual Studio' },
-    { imageUrl: '../../assets/tools/visual-studio-code.svg', name: 'Visual Studio Code' },
-    { imageUrl: '../../assets/tools/git.svg', name: 'Git' },
-    { imageUrl: '../../assets/tools/github.svg', name: 'GitHub' },
-    { imageUrl: '../../assets/tools/bitbucket.svg', name: 'BitBucket' },
-  ];
+  tools: Tool[] = [
+     { imageUrl: '../../assets/tools/figma.svg', name: 'Figma' },
+     { imageUrl: '../../assets/tools/visual-studio.svg', name: 'Visual Studio' },
+     { imageUrl: '../../assets/tools/visual-studio-code.svg', name: 'Visual Studio Code' },
+     { imageUrl: '../../assets/tools/git.svg', name: 'Git' },
+     { imageUrl: '../../assets/tools/github.svg', name: 'GitHub' },
+     { imageUrl: '../../assets/tools/bitbucket.png', name: 'BitBucket' },
+     { imageUrl: '../../assets/tools/jira.svg', name: 'Jira' },
+     { imageUrl: '../../assets/tools/confluence.svg', name: 'Confluence' },
+     { imageUrl: '../../assets/tools/azure-data-studio.svg', name: 'Azure Data Studio' },
+     { imageUrl: '../../assets/tools/vmware-horizon.png', name: 'VMware Horizon' },
+     { imageUrl: '../../assets/tools/azure-devops.png', name: 'Azure DevOps' },
+     { imageUrl: '../../assets/tools/azure.webp', name: 'Azure' },
+     { imageUrl: '../../assets/tools/notion.svg', name: 'Notion' },
+     { imageUrl: '../../assets/tools/figma.svg', name: 'Figma' },
+     { imageUrl: '../../assets/tools/miro.webp', name: 'Mirro' },
+     { imageUrl: '../../assets/tools/iis.png', name: 'IIS' },
+     { imageUrl: '../../assets/tools/teams.svg', name: 'Teams' },
+     { imageUrl: '../../assets/tools/npm.svg', name: 'npm' },
+     { imageUrl: '../../assets/tools/jenkins.svg', name: 'Jenkins' },
+     { imageUrl: '../../assets/tools/docker.svg', name: 'Docker' },
+   ];
   constructor() { }
 
   ngOnInit(): void {
