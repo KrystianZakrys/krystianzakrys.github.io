151871dd331314b4da8fadda7a05b7807af007ca
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-12 23:09:57 +0100

    Revamp

--- krystian-zakrys/src/app/app.module.ts
+++ krystian-zakrys/src/app/app.module.ts
@@ -3,44 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HelloComponent } from './hello/hello.component';
-import { SkillsComponent } from './skills/skills.component';
-import { YoExpComponent } from './yo-exp/yo-exp.component';
-import { TimelineComponent } from './timeline/timeline.component';
 import { DateDifferencePipe } from './date-difference.pipe';
-import { YoExpSmallComponent } from './yo-exp-small/yo-exp-small.component';
-import { ContactMeComponent } from './contact-me/contact-me.component';
 import { ProjectsCarouselleComponent } from './projects-carouselle/projects-carouselle.component';
-import { FooterComponent } from './footer/footer.component';
-import { SocialMediaComponent } from './social-media/social-media.component';
-import { HelloMobileComponent } from './hello-mobile/hello-mobile.component';
-import { HelloContactMeComponent } from './hello-contact-me/hello-contact-me.component';
-import { CertificatesComponent } from './certificates/certificates.component';
-import { ToolsComponent } from './tools/tools.component';
-import { ToolsMobileComponent } from './tools-mobile/tools-mobile.component';
-import { MoreFunLeverComponent } from './more-fun-lever/more-fun-lever.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HelloComponent,
-    SkillsComponent,
-    YoExpComponent,
-    TimelineComponent,
     DateDifferencePipe,
-    YoExpSmallComponent,
-    ContactMeComponent,
     ProjectsCarouselleComponent,
-    FooterComponent,
-    SocialMediaComponent,
-    HelloMobileComponent,
-    HelloContactMeComponent,
-    CertificatesComponent,
-    ToolsComponent,
-    ToolsMobileComponent,
-    MoreFunLeverComponent
   ],
   imports: [
     BrowserModule,
--- krystian-zakrys/src/app/projects-carouselle/projects-carouselle.component.ts
+++ krystian-zakrys/src/app/projects-carouselle/projects-carouselle.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
 
 export interface ProjectDto {
   title: string;
@@ -19,20 +17,37 @@ export interface ProjectDto {
 export class ProjectsCarouselleComponent implements OnInit {
   projects: ProjectDto[] = [
     {
-      title: 'Title',
-      desc: 'Description',
-      github: 'urlGithub',
-      imageUrl: '../../assets/photo2.jpg',
-      techStack: ['C#','SQL'],
-      url: 'url'
-    }, {
-      title: 'Title2',
-      desc: 'Description',
-      github: 'urlGithub',
-      imageUrl: '../../assets/photo2.jpg',
-      techStack: ['C#','SQL'],
-      url: 'url'
+      title: 'Bank AlJazira Website Facelift',
+      desc: 'I was responsible for recreating and modernising Bank AlJazira landing page and others facelift.',
+      github: '',
+      imageUrl: '../../assets/projects/aljazira.jpg',
+      techStack: ['DNN','C#','SQL', 'HTML', 'CSS','JS','jQuery'],
+      url: 'https://www.bankaljazira.com/en-us'
     },
+    {
+      title: 'Business Account Opening in Bank AlJazira',
+      desc: 'An onboarding application for business entities.',
+      github: '',
+      imageUrl: '../../assets/projects/bao.jpg',
+      techStack: ['C#', '.NET','SQL', 'HTML', 'CSS','JS','Angular'],
+      url: 'https://corporate.bankaljazira.com/cportal/BAO'
+    },
+    {
+      title: 'Altkom Akademia',
+      desc: 'I was working on a backoffice application and frontoffice application.',
+      github: '',
+      imageUrl: '../../assets/projects/akademia.jpg',
+      techStack: ['C#', '.NET','SQL', 'HTML', 'CSS','JS','Angular'],
+      url: 'https://www.altkomakademia.pl/'
+    },
+    {
+      title: 'PKO Leasing',
+      desc: 'I was working on a microservices system in .NET with microfrontends in Angular.',
+      github: '',
+      imageUrl: '../../assets/projects/pkol.jpg',
+      techStack: ['C#', '.NET','SQL', 'HTML', 'CSS','JS','Angular'],
+      url: 'https://www.pkoleasing.pl/'
+    }
   ];
 
   constructor() { }
